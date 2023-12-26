7b8cfca69f0882c1acb1034b896668da0c436a4f
ashwabu <abdulazizredi90@gmail.com>
2023-12-26 17:52:57 +0300

    db and app

--- ethio_therapy_app/settings.py
+++ ethio_therapy_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'ethio_therapy_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'dcrm',
+        'USER': 'root',
+        'PASSWORD': 'password123',
+        'hOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
