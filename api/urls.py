cf1b417980b090a4e7da1310125683028e46d97e
ashwabu <abdulazizredi90@gmail.com>
2023-12-26 20:25:56 +0300

    generics view

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from api import views
+
 urlpatterns = [
-   
+   path('',views.userList.as_view(),name='userList'),
+   path('/<int:pk>',views.userDetails.as_view(),name='userDetails'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,13 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
-
+from .serializer import UserSerializer
 # Create your views here.
+
+class userList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class userDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
