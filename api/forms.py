a70206515e9aef03451f28a1ad1fc4bc8fafe90f
ashwabu <abdulazizredi90@gmail.com>
2023-12-27 09:52:41 +0300

    form error

--- api/forms.py
+++ api/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .models import User
+from .models import CustomUser
 
 class UserRegistrationForm(UserCreationForm):
     class Meta:
-        model = User
+        model = CustomUser
         fields = ['username', 'password1', 'password2']
 
 class UserLoginForm(AuthenticationForm):
     class Meta:
-        model = User
+        model = CustomUser
