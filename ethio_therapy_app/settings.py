4f215e928f8c55bb2b94d59f4ccc04da7d2a1eb1
ashwabu <abdulazizredi90@gmail.com>
2023-12-27 10:02:51 +0300

    dose not work

--- ethio_therapy_app/settings.py
+++ ethio_therapy_app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,8 +56,10 @@ ROOT_URLCONF = 'ethio_therapy_app.urls'
 
 TEMPLATES = [
     {
+        
+        
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'api', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
