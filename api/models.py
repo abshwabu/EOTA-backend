3d4fa475ed3e1f8993973fcfa5d30476d8c1bcf6
ashwabu <abdulazizredi90@gmail.com>
2023-12-27 09:33:28 +0300

    custom user

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # Add additional fields if needed
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
