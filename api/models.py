ad27acc413592e1dfd4b008a729fff1141b4b406
ashwabu <abdulazizredi90@gmail.com>
2023-12-27 09:36:25 +0300

    error handling

--- api/models.py
+++ api/models.py
@@ -1,10 +1,24 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class CustomUser(AbstractUser):
     # Add additional fields if needed
     date_of_birth = models.DateField(null=True, blank=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+
+    # Provide unique related_name values for groups and user_permissions
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_groups',
+        related_query_name='custom_user_group',
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions',
+        related_query_name='custom_user_permission',
+        blank=True,
+    )
 
     def __str__(self):
         return self.username
