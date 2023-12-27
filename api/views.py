ca086742ed40e1e42aba3b7f027990a6e7db0c7a
ashwabu <abdulazizredi90@gmail.com>
2023-12-27 09:43:40 +0300

    views

--- api/views.py
+++ api/views.py
@@ -1,13 +1,30 @@
-from django.contrib.auth.models import User
-from rest_framework import generics
-from .serializer import UserSerializer
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import UserRegistrationForm, UserLoginForm
 
-class userList(generics.ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Redirect to the home page after registration
+    else:
+        form = UserRegistrationForm()
 
-class userDetails(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+    return render(request, 'registration/register.html', {'form': form})
 
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirect to the home page after login
+    else:
+        form = UserLoginForm()
+
+    return render(request, 'registration/login.html', {'form': form})
