99012652a0ff747fd0b3dd1b63272f5ad3ae4e8e
ashwabu <abdulazizredi90@gmail.com>
2023-12-29 16:40:03 +0300

    record model

--- api/models.py
+++ api/models.py
@@ -1,24 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import User
 
-class CustomUser(AbstractUser):
-    # Add additional fields if needed
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
-
-    # Provide unique related_name values for groups and user_permissions
-    groups = models.ManyToManyField(
-        'auth.Group',
-        related_name='custom_user_groups',
-        related_query_name='custom_user_group',
-        blank=True,
-    )
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        related_name='custom_user_permissions',
-        related_query_name='custom_user_permission',
-        blank=True,
-    )
+class Record(models.Model):
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50,null=True, blank=True)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15,null=True, blank=True)
+    email = models.EmailField(max_length=50,null=True, blank=True)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    address=models.CharField(max_length=100)
 
     def __str__(self):
-        return self.username
+        return(f"{self.first_name} {self.last_name}")
