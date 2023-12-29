ba1530460dfd8219d6c0a0399ee26496d5bc36fd
ashwabu <abdulazizredi90@gmail.com>
2023-12-29 19:15:44 +0300

    urls

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from .views import register, additional_info
 
 urlpatterns = [
     path('register/', register, name='register'),
-    path('additional_info/<int:user_id>/', additional_info, name='additional_info'),
+    path('additional_info/<int:user_id>/', additional_info, name='additional_info'),  # Ensure the name matches
     # Add other URLs as needed
 ]
