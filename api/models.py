517c000b3b1ceb0e356944c1d587cb94de19509f
ashwabu <abdulazizredi90@gmail.com>
2023-12-29 18:58:51 +0300

    model 3.5

--- api/models.py
+++ api/models.py
@@ -1,27 +1,13 @@
-from django.contrib.auth.models import AbstractUser
+# models.py
 from django.db import models
-from django.contrib.auth.models import User
 
-class Record(models.Model):
-    ISLAM = 'ISL'
-    CRISTIAN ='CRI'
-    JEW ='jew'
-    TRADITIONAL = 'TRD'
-    RELIGION = [
-        (ISLAM, 'islam'),
-        (CRISTIAN,'cristian'),
-        (TRADITIONAL, 'traditional'),
-        (JEW, 'jew'),
-    ]
-    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    first_name = models.CharField(max_length=50,null=True, blank=True)
-    last_name = models.CharField(max_length=50)
-    phone = models.CharField(max_length=15,null=True, blank=True)
-    email = models.EmailField(max_length=50,null=True, blank=True)
-    city = models.CharField(max_length=50)
-    state = models.CharField(max_length=50)
-    address=models.CharField(max_length=100)
+class UserProfile(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=50)
+    name = models.CharField(max_length=100, blank=True)
+    age = models.IntegerField(blank=True, null=True)
+    occupation = models.CharField(max_length=50, blank=True)
+    religion = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
-        return(f"{self.first_name} {self.last_name}")
+        return self.username
