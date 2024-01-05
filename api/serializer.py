6872695792bed8d9d34c6b4a3ff5d4fd5e335b1f
ashwabu <abdulazizredi90@gmail.com>
2024-01-05 18:03:20 +0300

    normal

--- api/serializer.py
+++ api/serializer.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id','username','password','first_name','last_name','email']
