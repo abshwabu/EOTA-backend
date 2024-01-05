2733eaf586f0db9a48a0e561f1e44a046af9ef57
ashwabu <abdulazizredi90@gmail.com>
2024-01-05 07:43:12 +0300

    home

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('',views.home , name='home'),
     path('register/', views.register, name='register'),
     path('user_profile/', views.user_profile, name='user_profile'),
 ]
