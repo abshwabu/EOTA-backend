475275b74e1ec7774d53cca4c3027702cfa3fe22
ashwabu <abdulazizredi90@gmail.com>
2024-01-05 08:04:58 +0300

    choice

--- api/forms.py
+++ api/forms.py
@@ -3,9 +3,13 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import UserProfile
 from django.contrib.auth.models import User
 class RegistrationForm(UserCreationForm):
+    Bool_options = [{True:'Ok'}, {False:'No thanks'}]
+    Enter_more_details = forms.BooleanField(
+        widget=forms.RadioSelect(choices=Bool_options)
+    )
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ['username', 'password1', 'password2','Enter_more_details']
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
