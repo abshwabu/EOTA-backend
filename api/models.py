27bf2a9724d0b5b80dcc78a6081cf7e69aab7eca
ashwabu <abdulazizredi90@gmail.com>
2024-01-05 07:42:51 +0300

    user profile

--- api/models.py
+++ api/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
     name = models.CharField(max_length=100)
     age = models.IntegerField(choices=[
         (18, '18-24'),
         (25, '25-34'),
         (35, '35-44'),
-        # Add more age ranges as needed
+        
     ])
     occupation = models.CharField(max_length=50, choices=[
         ('student', 'Student'),
